7a8dc5e53d85d38df517e82a75c88a2528da4ca5
MxRey <reygemz07@gmail.com>
2023-10-19 14:15:42 +0700

    1-8

--- index.js
+++ index.js
@@ -4,18 +4,78 @@ const app = express()
 const port = 4444
 
 app.get('/', (req, res) => {
-    res.json({ message: "Selamat datang" });
+    res.json({ message: "Ini JSON" });
 })
 
+// POST method
 app.post('/', (req, res) => {
-    res.send('Selamat datang')
+    res.send('Ini POST')
 })
 
-app.put('/kopi', (req, res) => {
-    res.send('Selamat Datang')
+// PUT method
+app.put('/estea', (req, res) => {
+    res.send('Aku suka ice tea')
 })
 
-app.delete('/kopi', (req, res) => {
-    res.send('Selamat datang')
+// DELETE method
+app.delete('/estea', (req, res) => {
+    res.send('Delete kang')
 });
- //
+
+ // representasi data dari database
+ const data_users = [
+    { id: 1, name: "Azka Dapin", alamat: "Nagarta" },
+    { id: 2, name: "Ghani Rasya", alamat: "Surakarta" },
+    { id: 3, name: "Hana Zain", alamat: "Jakarta" },
+    { id: 4, name: "Ibrahim", alamat: "Bandung" },
+];
+
+app.get("/users", (req, res) => {
+    //mendapatkan data dari database
+    const data = data_users;
+
+    //memberikan respon json data
+    let result = {
+        status: 200,
+        data: data,
+    };
+
+    res.json(result);
+});
+
+// jadi dari 1 dst
+app.get("/users/:id", (req, res) => {
+    // get data dari parameter
+    let id = parseInt(req.params.id);
+
+    // get data dari database
+    let result;
+    const user = data_users.find((user) => user.id === id);
+    if (user) {
+        result = {
+        status: 200,
+        data: user,
+        };
+    }
+    res.json(result);
+});
+
+app.get("/users/:id", (req, res) => {
+    // get data dari parameter
+    let id = parseInt(req.params.id);
+
+    // get data dari database
+    let result;
+    const user = data_users.find((user) => user.id === id);
+    if (user) {
+        result = {
+        status: 200,
+        data: user,
+        };
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+    res.json(result);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
